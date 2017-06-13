c8048156005805c8b66dfb3a6d14a747718ad7cc
Gabrielius <gtamavicius@gmail.com>
2017-06-13 14:41:51 +0200

    first commit

---
+++ models/user.js
@@ -0,0 +1,55 @@
+
+var mongoose = require ('mongoose');
+var bcrypt = require ('bcryptjs');
+mongoose.connect ('mongodb://localhost/nodeauth');
+
+var db = mongoose.conncetion; //variable holding the connection
+
+//Defining User Schema 
+
+var UserSchema = mongoose.Schema ({ 
+	username: {
+		type: String,
+		index:true
+	},
+	password: {
+		type:String
+	},
+	email: {
+		type:String
+	},
+	name: {
+		type:String
+	},
+
+	profileimage: {
+		type:String
+	}
+});
+
+var User = module.exports = mongoose.model ('User', UserSchema); //alows to use the models outside 
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	var query = {username:username};
+	User.findOne(query, callback);
+}
+
+module.exports.comparePassowords = function (candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    callback(null, isMatch);
+});
+}
+
+module.exports.createUser =  function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash("newUser.password", salt, function(err, hash) {
+        newUser.password = hash;
+        newUser.save(callback);
+    });
+});
+}
+
